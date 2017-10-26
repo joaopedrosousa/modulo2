423afc2af890c82abe3103c6055224823eff451d
joaopedrosousa <32169066+joaopedrosousa@users.noreply.github.com>
2017-10-26 17:11:03 +0100

    Add files via upload

---
+++ operaçõesaritmeticas.java
@@ -0,0 +1,12 @@
+package operaçõesaritmeticas;
+
+public class operaçõesaritmeticas {
+
+	public static void main(String[] args) {
+		System.out.printf("%d\n" ,4+3*(15-2));
+		System.out.printf("%f\n" ,40.0/9.0);
+		System.out.printf("%d\n", 40%3); 
+
+	}
+
+}

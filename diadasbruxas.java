a0dff02c6da7e427c2f8060a7aebe167a731706e
joaopedrosousa <32169066+joaopedrosousa@users.noreply.github.com>
2017-11-09 15:28:02 +0000

    Add files via upload

---
+++ diadasbruxas.java
@@ -0,0 +1,17 @@
+package diadasbruxas;
+
+import java.io.*;
+
+public class diadasbruxas {
+
+	public static void main(String[] args) {
+		InputStreamReader LerS = new InputStreamReader (System.in);
+		String C = "";
+		System.out.println("Dia das Bruxas");
+		System.out.println("Doce ou travessura?");
+		InputStreamReader LerSL = new InputStreamReader (System.in);
+		System.out.println("O Utilizador escolheu" + C + ".");
+
+	}
+
+}

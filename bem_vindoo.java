23f6a5fb9cb1006a9c007035eae4c65c43351ded
joaopedrosousa <32169066+joaopedrosousa@users.noreply.github.com>
2017-11-24 15:12:00 +0000

    Add files via upload

---
+++ bem_vindoo.java
@@ -0,0 +1,11 @@
+package bem_vindo;
+
+public class bem_vindoo {
+
+	public static void main(String[] args) {
+	final String mensagem = "Hoje é sexta-feira";
+	for(int I =1 ; I <= 5 ; I ++)
+    System.out.println(mensagem);
+	}
+
+}

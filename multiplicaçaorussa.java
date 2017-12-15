95721b8e3bcaacb52443d819a422eec6306b26a3
joaopedrosousa <32169066+joaopedrosousa@users.noreply.github.com>
2017-12-15 17:28:06 +0000

    Add files via upload

--- multiplicaçaorussa.java
+++ multiplicaçaorussa.java
@@ -3,16 +3,28 @@ import java.util.Scanner;
 public class multiplicaçaorussa {
     public static void main(String[] args) {
   
-        int M , m = 0, m1 , M1 , r;
-        Scanner dados=new Scanner(System.in);
-        System.out.print("Qual é o multiplicando?");
-        m=dados.nextInt();
-        System.out.print("Qual é o multiplicador?");
-        M=dados.nextInt();
-        while (m>1) {
-        m=m/2;
-        System.out.print(m + "\n");
-        
-        }
-    }    
-}
+    	  int M , m , m1 , M1 , r=0 ;
+          Scanner dados=new Scanner(System.in);
+          System.out.print("Qual é o multiplicando ? ");
+          m=dados.nextInt();
+          System.out.print("Qual é o multiplicador ? ");
+          M=dados.nextInt();
+      m1=m;
+      M1=M;
+           System.out.print(m + "\t"); 
+           System.out.print(M + "\n");         
+          while (m1>1){
+           m1=m1/2;
+           System.out.print(m1 + "\t"); 
+              M1=M1*2;
+      System.out.print(M1 + "\n"); 
+          if (m1%2!=0)
+              r=r+M1;
+          }
+          if(M%2!=0){
+           r=r+M; 
+           }
+       System.out.print("O resultado é " + r);  
+      }
+
+  }
